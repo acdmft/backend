606bc5b602742be81924bedfa5ac15026d5d1313
fusbudget <andrewsanser@gmail.com>
2022-03-07 17:32:18 +0100

    add axios, solution to bonus-1

---
+++ chapitre_01/bonus-01/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const res = require("express/lib/response");
+
+const countries = require("./dataCountries");
+
+// Routes
+app.get("/countries/all", (req, res) => {
+  const data = countries.map((country) => {
+    return country.name.common;
+  });
+  res.json(data);
+});
+
+// Axios request
+let countriesNames = [];
+
+// function getCountries() {
+  axios.get("http://localhost:8080/countries/all")
+  .then((res) => {
+    console.log(res.data);
+    });
+// }
+// getCountries();
+
+app.listen(8080, () => {
+  console.log("Listen on port 8080");
+});
