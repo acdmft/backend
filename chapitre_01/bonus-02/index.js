1bb267c7f1e5becd06981f2a60bdb9dfbe806a16
fusbudget <andrewsanser@gmail.com>
2022-03-10 15:17:30 +0100

    add route to get pokemon by name

--- chapitre_01/bonus-02/index.js
+++ chapitre_01/bonus-02/index.js
@@ -4,25 +4,34 @@ const axios = require("axios");
 
 app.use(express.json());
 
-// POKEMONS 
+// POKEMONS
 const pokemons = [];
-// async function getPokemons() {
-//   let result = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=26&offset=26");
-//   pokemons.push(result.data);
-// } 
-// getPokemons();
-axios.get("https://pokeapi.co/api/v2/pokemon?limit=26&offset=26")
+axios
+  .get("https://pokeapi.co/api/v2/pokemon?limit=26&offset=26")
   .then((res) => {
-    pokemons.push(res.data);
+    pokemons.push(res.data.results);
   });
-//ROUTES 
-// GET ALL POKEMONS 
+console.log(pokemons);
+
+//ROUTES
+// GET ALL POKEMONS
 app.get("/all", (_req, res) => {
   res.send(pokemons);
+});
+// GET POKEMON BY ID
+app.get("/id/:id", (req, res) => {
+  const pokemon = pokemons[0].find((pokemon, index) => {
+    return index === parseInt(req.params.id);
+  });
+
+  if (!pokemon) {
+    res.send(`Pokemon with id: ${req.params.id} not found`);
+  }
+  res.send(pokemon.name);
 });
 
 app.get("*", (_req, res) => {
   res.status(404).send("Page not found");
-})
+});
 
 app.listen(8080, () => console.log("Listen port 8080..."));
