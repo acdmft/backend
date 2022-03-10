29b713f290fe0e063f66b5d8d4574dd3208b29e5
fusbudget <andrewsanser@gmail.com>
2022-03-10 10:41:09 +0100

    add route to get country by name in bonus-01

--- chapitre_01/bonus-01/index.js
+++ chapitre_01/bonus-01/index.js
@@ -6,28 +6,40 @@ const res = require("express/lib/response");
 const countries = require("./dataCountries");
 
 // Routes
-app.get("/countries/all", (req, res) => {
+// GET ALL COUNTRIES
+app.get("/countries/all", (_req, res) => {
   const data = countries.map((country) => {
     return country.name.common;
   });
   res.json(data);
+});
+// GET COUNTRY BY NAME
+app.get("/countries/name/:name", (req, res) => {
+  const country = countries.find((country) => {
+    return (
+      country.name.common.replace(" ", "-").toLowerCase() ===
+      req.params.name.replace(" ", "-").toLowerCase()
+    );
+  });
+  if (country) {
+    return res.json(country);
+  }
+  res.send("Country not found");
 });
 
 // Axios request
 let countriesNames = [];
 
 // function getCountries() {
-  axios.get("http://localhost:8080/countries/all")
-  .then((res) => {
-    console.log(res.data);
-    });
+axios.get("http://localhost:8080/countries/all").then((res) => {
+  console.log(res.data);
+});
 // }
 // getCountries();
 
-app.listen('*', (_req, res) => {
-  res.status(404).send("Page not found");
+app.use("*", (err, req, res, next) => {
+  res.send("Error");
 });
-
 
 app.listen(8080, () => {
   console.log("Listen on port 8080");
